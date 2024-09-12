bf41655f2c295dd6561976286e0e6a0acf3b1139
Maxim <fixor@yandex.ru>
2024-09-12 20:48:23 +0300

    Sprint 12 fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO RATING_MPA (MPA_ID, MPA_NAME)
+MERGE INTO RATING_MPA (MPA_ID, MPA_NAME)
     VALUES (1, 'G'),
     (2, 'PG'),
     (3, 'PG-13'),
     (4, 'R'),
     (5, 'NC-17');
 
-INSERT INTO GENRE (GENRE_ID, GENRE_NAME)
+MERGE INTO GENRE (GENRE_ID, GENRE_NAME)
     VALUES (1, 'Комедия'),
     (2, 'Драма'),
     (3, 'Мультфильм'),
